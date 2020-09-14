20a9b6b255c210dd63b85c48358b5c5046fc660a
Lauren Jenkinson <lauren@kat.net.nz>
2020-09-14 16:58:07 +1200

    usersettings/mfa: Allow disabling MFA

--- app/controllers/user_settings_mfa_controller.rb
+++ app/controllers/user_settings_mfa_controller.rb
@@ -1,5 +1,7 @@
 class Kukupa::Controllers::UserSettingsMfaController < Kukupa::Controllers::ApplicationController
   add_route :get, "/"
+  add_route :get, "/disable", method: :disable
+  add_route :post, "/disable", method: :disable
 
   def before
     return halt 404 unless logged_in?
@@ -30,5 +32,46 @@ class Kukupa::Controllers::UserSettingsMfaController < Kukupa::Controllers::Appl
         mfa: @user_mfa,
       }
     end
+  end
+
+  def disable
+    if @user_mfa[:has_roles]
+      flash :error, t(:'usersettings/mfa/disable/errors/has_roles')
+      return redirect url('/user/mfa')
+    end
+
+    if request.post?
+      password = request.params['password']&.strip
+      password = nil if password&.empty?
+      if @user.password_correct?(password)
+        # Disable TOTP
+        @user.totp_enabled = false
+        @user.totp_secret = nil
+
+        # TODO: Destroy security key entries
+
+        # Invalidate recovery codes
+        Kukupa::Models::Token.where(
+          user_id: @user.id,
+          use: 'mfa_recovery',
+          valid: true,
+        ).update(valid: false)
+
+        # Save user
+        @user.save
+
+        flash :success, t(:'usersettings/mfa/disable/success')
+        return redirect url('/user/mfa')
+
+      else
+        flash :error, t(:'usersettings/mfa/disable/errors/password_incorrect')
+      end
+    end
+
+    haml :'usersettings/mfa/disable', layout: :layout_minimal, locals: {
+      title: @title,
+      user: @user,
+      mfa: @user_mfa,
+    }
   end
 end
