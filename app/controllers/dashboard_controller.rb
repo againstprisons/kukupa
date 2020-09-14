3785ddcb418a00f85aa350e5016d32c6e909d608
Lauren Jenkinson <lauren@kat.net.nz>
2020-09-14 20:55:32 +1200

    dashboard: Add list of assigned cases

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,13 +11,23 @@ class Kukupa::Controllers::DashboardController < Kukupa::Controllers::Applicatio
     @user = current_user
     @user_name = @user.decrypt(:name)
     @user_name = nil if @user_name.nil? || @user_name&.empty?
+    @my_cases = Kukupa::Models::Case.where(assigned_advocate: @user.id).map do |c|
+      url = Addressable::URI.parse(url("/case/#{c.id}"))
+
+      {
+        :case => c,
+        :name => c.get_name,
+        :url => url.to_s,
+      }
+    end
 
     return haml(:'dashboard/index', :locals => {
       :title => @title,
       :user => {
         :user => @user,
         :name => @user_name,
-      }
+      },
+      cases: @my_cases,
     })
   end
 end
