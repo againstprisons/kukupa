1e8a26c57408fee20db6b0d62dab611e4e1da9ad
Lauren Jenkinson <lauren@kat.net.nz>
2020-09-15 22:33:02 +1200

    models/CaseSpendYear: Use floats, don't encrypt/index the year

--- app/models/case_spend.rb
+++ app/models/case_spend.rb
@@ -9,24 +9,22 @@ class Kukupa::Models::CaseSpendYear < Sequel::Model
     c = c.id if c.respond_to?(:id)
 
     # construct a hash where the key is the year, as a string, and the value
-    # is an integer of the spend for that year
+    # is a float of the spend for that year
     years = {}
     Kukupa::Models::CaseSpend.where(case: c).each do |spend|
-      years[spend.get_year] ||= 0
-      years[spend.get_year] += spend.decrypt(:amount).to_i
+      years[spend.get_year] ||= 0.0
+      years[spend.get_year] += spend.decrypt(:amount).to_f
     end
 
     # destroy the existing aggregates for this case
     self.where(case: c).delete
 
-    # create new aggregates
+    # create new aggregates, converting the amount to an integer
     years.keys.map do |year|
-      ag = self.new(case: c)
+      ag = self.new(case: c, year: year)
       ag.save # save to get ID
 
-      ag.year_search = Kukupa::Crypto.index('CaseSpendYear', 'year_search', year)
-      ag.encrypt(:year, year)
-      ag.encrypt(:amount, years[year])
+      ag.encrypt(:amount, years[year].to_i)
       ag.save
 
       ag
@@ -36,8 +34,7 @@ class Kukupa::Models::CaseSpendYear < Sequel::Model
   def self.get_case_year(c, year)
     c = c.id if c.respond_to?(:id)
     year = year.strftime("%Y") if year.respond_to?(:strftime)
-    year_search = Kukupa::Crypto.index('CaseSpendYear', 'year_search', year)
 
-    self.where(case: c, year_search: year_search).first
+    self.where(case: c, year: year).first
   end
 end
