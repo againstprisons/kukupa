a4d8df4bb829edd268becf1229a94f635b61bdf1
Lauren Jenkinson <lauren@kat.net.nz>
2020-09-19 23:26:47 +1200

    case/task/add: Allow adding new tasks

---
+++ app/controllers/case_task_add_controller.rb
@@ -0,0 +1,64 @@
+class Kukupa::Controllers::CaseTaskAddController < Kukupa::Controllers::CaseController
+  add_route :get, '/'
+  add_route :post, '/'
+
+  include Kukupa::Helpers::CaseHelpers
+
+  def before
+    return halt 404 unless logged_in?
+    @user = current_user
+  end
+
+  def index(cid)
+    @case = Kukupa::Models::Case[cid]
+    return halt 404 unless @case
+    unless has_role?('case:view_all')
+      return halt 404 unless @case.assigned_advocate == @user.id
+    end
+
+    @accessors = case_users_with_access(@case)
+    @case_name = @case.get_name
+    @title = t(:'case/task/add/title', name: @case_name)
+
+    if request.get?
+      return haml(:'case/task/add', :locals => {
+        title: @title,
+        case_obj: @case,
+        case_name: @case_name,
+        case_accessors: @accessors,
+      })
+    end
+
+    # get content
+    @content = request.params['content']&.strip
+    @content = nil if @content&.empty?
+    unless @content
+      flash :error, t(:'case/task/add/errors/no_content')
+      return redirect request.path
+    end
+
+    # get assignee
+    @assignee = Kukupa::Models::User[request.params['assignee'].to_i]
+    unless case_user_can_access?(@case, @assignee)
+      flash :error, t(:'case/task/add/errors/invalid_user')
+      return redirect request.path
+    end
+
+    # create task
+    @task = Kukupa::Models::CaseTask.new(
+      case: @case.id,
+      author: @user.id,
+      assigned_to: @assignee.id
+    ).save
+
+    @task.encrypt(:content, @content)
+    @task.save
+
+    # TODO: send "new task" email to the assigned advocate for this task
+    # if the assigned advocate was not the one that created this task
+
+    # redirect back
+    flash :success, t(:'case/task/add/success')
+    redirect url("/case/#{@case.id}/view#case-view-tasks")
+  end
+end
--- app/helpers/case_helpers.rb
+++ app/helpers/case_helpers.rb
@@ -12,4 +12,38 @@ module Kukupa::Helpers::CaseHelpers
 
     advocates
   end
+
+  def case_users_with_access(c)
+    c = c.id if c.respond_to?(:id)
+    c = Kukupa::Models::Case[c]
+
+    uids = []
+
+    # assigned advocate for this case
+    uids << c.assigned_advocate
+
+    # all users with `case:*` or `case:view_all` roles
+    uids << Kukupa::Models::UserRole.where(role: "case:*").map(&:user_id).to_a
+    uids << Kukupa::Models::UserRole.where(role: "case:view_all").map(&:user_id).to_a
+
+    # get user objects
+    advocates = {}
+    uids.flatten.compact.each do |uid|
+      advocates = case_populate_advocate(advocates, uid)
+    end
+
+    advocates.values.sort { |a, b| a[:id] <=> b[:id] }
+  end
+
+  def case_user_can_access?(c, u)
+    c = c.id if c.respond_to?(:id)
+    c = Kukupa::Models::Case[c]
+    u = u.id if u.respond_to?(:id)
+    u = Kukupa::Models::User[u]
+
+    return true if has_role?('case:view_all', user: u)
+    return true if c.assigned_advocate == u.id
+
+    false
+  end
 end
