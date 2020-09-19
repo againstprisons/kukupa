1cdc089a7887cb8f29e6a8bf8260cbbc785ccaf0
Lauren Jenkinson <lauren@kat.net.nz>
2020-09-20 02:46:39 +1200

    case/task/edit: Allow marking a task as complete

--- app/controllers/case_task_edit_controller.rb
+++ app/controllers/case_task_edit_controller.rb
@@ -1,6 +1,7 @@
 class Kukupa::Controllers::CaseTaskEditController < Kukupa::Controllers::CaseController
   add_route :get, '/'
   add_route :post, '/'
+  add_route :post, '/complete', method: :complete
 
   include Kukupa::Helpers::CaseHelpers
 
@@ -33,6 +34,9 @@ class Kukupa::Controllers::CaseTaskEditController < Kukupa::Controllers::CaseCon
         task_obj: @task,
         task_assignee: Kukupa::Models::User[@task.assigned_to],
         task_content: @task.decrypt(:content),
+        urls: {
+          complete: url("/case/#{@case.id}/task/#{@task.id}/complete"),
+        },
       })
     end
 
@@ -55,7 +59,7 @@ class Kukupa::Controllers::CaseTaskEditController < Kukupa::Controllers::CaseCon
       # if assignee changing, create update entry
       update_entry = Kukupa::Models::CaseTaskUpdate.new(
         task: @task.id,
-        author: @user.id
+        author: @user.id,
       ).save
 
       update_entry.encrypt(:update_type, 'assign')
@@ -73,5 +77,38 @@ class Kukupa::Controllers::CaseTaskEditController < Kukupa::Controllers::CaseCon
     # redirect back
     flash :success, t(:'case/task/edit/edit/success')
     redirect request.path
+  end
+
+  def complete(cid, tid)
+    @case = Kukupa::Models::Case[cid.to_i]
+    return halt 404 unless @case
+    unless has_role?('case:view_all')
+      return halt 404 unless @case.assigned_advocate == @user.id
+    end
+
+    @task = Kukupa::Models::CaseTask[tid.to_i]
+    return halt 404 unless @task
+    return halt 404 unless @task.case == @case.id
+
+    unless @task.completion.nil?
+      flash :error, t(:'case/task/edit/complete/errors/already_complete')
+      return redirect url("/case/#{@case.id}/task/#{@task.id}")
+    end
+
+    # set completion
+    @task.completion = DateTime.now
+    @task.save
+
+    # create update entry
+    update_entry = Kukupa::Models::CaseTaskUpdate.new(
+      task: @task.id,
+      author: @user.id,
+    ).save
+
+    update_entry.encrypt(:update_type, 'complete')
+    update_entry.save
+
+    flash :success, t(:'case/task/edit/complete/success', task_id: @task.id, ts: update_entry.creation)
+    redirect url("/case/#{@case.id}/view##{update_entry.anchor}")
   end
 end
