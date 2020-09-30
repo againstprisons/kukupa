21df2017189acb04cb499681d4cc89c7c205c309
Lauren Jenkinson <lauren@iris.ac.nz>
2020-09-30 19:13:53 +1300

    app: Move pre-flight checks into ApplicationController
    This removes the pre-flight checks from the Sinatra Kukupa::Application
    and puts them in ApplicationController, allowing us to have a separate
    lot of pre-flights for the API.

--- app/application.rb
+++ app/application.rb
@@ -7,19 +7,6 @@ class Kukupa::Application < Sinatra::Base
   set :haml, :format => :html5, :default_encoding => "UTF-8"
   enable :sessions
 
-  before do
-    # Check if maintenance mode
-    if is_maintenance? && !maintenance_path_allowed?
-      next halt 503, maintenance_render
-    end
-
-    # Set and check CSRF
-    csrf_set!
-    unless request.safe?
-      next halt 403, "CSRF failed" unless csrf_ok?
-    end
-  end
-
   not_found do
     haml :'errors/not_found', :layout => :layout_minimal, :locals => {
       :title => t(:'errors/not_found/title'),
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,19 @@ class Kukupa::Controllers::ApplicationController
       self.send(meth, *args, &bk)
     end
   end
+
+  def preflight
+    # Check if maintenance mode
+    if is_maintenance? && !maintenance_path_allowed?
+      return halt 503, maintenance_render
+    end
+
+    # Set and check CSRF
+    csrf_set!
+    unless request.safe? 
+      unless csrf_ok?
+        return halt 403, "CSRF failed"
+      end
+    end
+  end
 end
