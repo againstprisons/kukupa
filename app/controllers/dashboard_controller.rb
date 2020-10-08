f6604d98c42738f5626c3922e42751b52d023d12
Lauren Jenkinson <lauren@iris.ac.nz>
2020-10-08 13:27:10 +1300

    dashboard: Show incomplete tasks

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,6 +11,22 @@ class Kukupa::Controllers::DashboardController < Kukupa::Controllers::Applicatio
     @user = current_user
     @user_name = @user.decrypt(:name)
     @user_name = nil if @user_name.nil? || @user_name&.empty?
+
+    @my_tasks = Kukupa::Models::CaseTask.where(assigned_to: @user.id, completion: nil).map do |t|
+      c = Kukupa::Models::Case[t.case]
+      curl = Addressable::URI.parse(url("/case/#{c.id}/view#case-view-tasks"))
+      turl = Addressable::URI.parse(url("/case/#{c.id}/task/#{t.id}"))
+      content = Sanitize.fragment(t.decrypt(:content).to_s, Sanitize::Config::RESTRICTED)
+
+      {
+        case: c,
+        name: c.get_name,
+        curl: curl.to_s,
+        turl: turl.to_s,
+        content: content,
+      }
+    end
+
     @my_cases = Kukupa::Models::Case.where(assigned_advocate: @user.id).map do |c|
       url = Addressable::URI.parse(url("/case/#{c.id}/view"))
 
@@ -28,6 +44,7 @@ class Kukupa::Controllers::DashboardController < Kukupa::Controllers::Applicatio
         :name => @user_name,
       },
       cases: @my_cases,
+      tasks: @my_tasks,
     })
   end
 end
