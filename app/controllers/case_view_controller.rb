3f993a9e23e59b7b01d5ed1dba0c290ab18c0352
Lauren Jenkinson <lauren@iris.ac.nz>
2020-10-14 16:32:47 +1300

    case/view: Show some case details

--- app/controllers/case_view_controller.rb
+++ app/controllers/case_view_controller.rb
@@ -21,6 +21,16 @@ class Kukupa::Controllers::CaseViewController < Kukupa::Controllers::CaseControl
     @renderables = get_renderables(@case)
     @tasks_complete = request.params['tc'].to_i.positive?
     @tasks = get_tasks(@case, include_complete: @tasks_complete)
+    @prison = Kukupa::Models::Prison[@case.decrypt(:prison).to_i]
+    if @prison && @case.prisoner_number
+      p_addr = @prison.decrypt(:physical_address)
+      p_addr = p_addr.split('\n').map(&:strip)
+      @address = [
+        @case_name,
+        "PRN #{@case.decrypt(:prisoner_number)&.strip}",
+        p_addr,
+      ].join(', ')
+    end
 
     @spend_year_max = Kukupa.app_config['fund-max-spend-per-case-year'].to_f
     @spend_year = Kukupa::Models::CaseSpendAggregate.get_case_year_total(@case, DateTime.now)
@@ -30,6 +40,8 @@ class Kukupa::Controllers::CaseViewController < Kukupa::Controllers::CaseControl
       title: @title,
       case_obj: @case,
       case_name: @case_name,
+      case_prison: @prison,
+      case_address: @address,
       renderables: @renderables,
       tasks: @tasks,
       tasks_complete: @tasks_complete,
