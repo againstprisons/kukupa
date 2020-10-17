c9e7fd95cb97d2f0bdde3a21a20800887fd7f48e
Lauren Jenkinson <lauren@iris.ac.nz>
2020-10-17 13:33:18 +1300

    case/index: Remove statistics (to be reimplemented later)

--- app/controllers/case_index_controller.rb
+++ app/controllers/case_index_controller.rb
@@ -16,22 +16,6 @@ class Kukupa::Controllers::CaseIndexController < Kukupa::Controllers::CaseContro
       view_all: has_role?('case:view_all'),
     )
 
-    # if we have 'case:stats:view' permission:
-    #   - show total spending for last month
-    #   - show total spending for this month
-    if has_role?('case:stats:view')
-      # TODO: don't use chronic here
-      @last_month_spend = Kukupa::Models::CaseSpendAggregate.get_month_total(Chronic.parse('last month'))
-      @this_month_spend = Kukupa::Models::CaseSpendAggregate.get_month_total(DateTime.now)
-
-      @stats = {
-        spend: {
-          last: @last_month_spend,
-          current: @this_month_spend,
-        },
-      }
-    end
-
     @title = t(:'case/index/title')
     return haml(:'case/index', :locals => {
       title: @title,
