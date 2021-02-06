abc71680ec63e076da319160215abc5700018acb
Lauren Jenkinson <lauren@iris.ac.nz>
2021-02-07 00:59:32 +1300

    case/list: Allow sorting by case purpose

--- app/helpers/case_list_helpers.rb
+++ app/helpers/case_list_helpers.rb
@@ -1,4 +1,6 @@
 module Kukupa::Helpers::CaseListHelpers
+  # This function returns a hash, where the key is the translation key for
+  # the portion of the case list (the value of that key).
   def case_list_get_cases(opts = {})
     opts[:sort] ||= :assigned
     opts[:is_open] ||= true
@@ -23,6 +25,7 @@ module Kukupa::Helpers::CaseListHelpers
         :open => c.is_open,
         :prison => prisons[prison.to_s],
         :prn => c.decrypt(:prisoner_number),
+        :purpose => c.purpose,
       }
     end
 
@@ -42,12 +45,29 @@ module Kukupa::Helpers::CaseListHelpers
       c
     end
 
-    # perform the sort
-    # TODO: allow sort options other than assigned/unassigned
+    # sort by whether cases have an assigned advocate - unassigned at the top
     cases.sort! do |a, b|
       (a[:advocates].empty?() ? 0 : 1) <=> (b[:advocates].empty?() ? 0 : 1)
     end
 
-    cases
+    case opts[:sort]
+    when :purpose
+      purposes = {}
+      cases.each do |c|
+        purposes["case_purpose/#{c[:purpose]}".to_sym] ||= []
+        purposes["case_purpose/#{c[:purpose]}".to_sym] << c
+      end
+
+      purposes
+
+    else # when :assigned
+      assigned = cases.reject { |x| x[:advocates].empty? }
+      unassigned = cases.filter { |x| x[:advocates].empty? }
+
+      {
+        :'case/list/sort/assigned/unassigned' => unassigned,
+        :'case/list/sort/assigned/assigned' => assigned,
+      }
+    end
   end
 end
