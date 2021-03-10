c547edf6e3c22185e8b712bf5cbbf663695295c7
Lauren Jenkinson <lauren@iris.ac.nz>
2021-03-10 13:31:45 +0000

    case: Block access to case if case type is invalid

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -2,5 +2,16 @@ class Kukupa::Controllers::CaseController < Kukupa::Controllers::ApplicationCont
   def before(*args)
     @user = current_user
     @case = Kukupa::Models::Case[args.first.to_i]
+    if @case
+      # bail on unknown type
+      unless Kukupa::Models::Case::ALLOWED_TYPES.include?(@case.type)
+        out = haml(:'case/errors/unknown_type', layout: :layout_minimal, locals: {
+          title: t(:'case/unknown_type/title'),
+          case_obj: @case,
+        })
+
+        halt out
+      end
+    end
   end
 end
