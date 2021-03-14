6f970ade71de77a91302d4746fe5a6fb1ee6cdf5
Lauren Jenkinson <lauren@iris.ac.nz>
2021-03-14 17:30:56 +1300

    case/timeline: Show incomplete tasks in timeline (sorted by deadline)

--- app/helpers/case_timeline_helpers.rb
+++ app/helpers/case_timeline_helpers.rb
@@ -19,14 +19,39 @@ module Kukupa::Helpers::CaseTimelineHelpers
     }
   end
 
+  def timeline_entry_for_task(task)
+    assignee = Kukupa::Models::User[task.assigned_to]
+
+    {
+      is_task: true,
+      obj: task,
+      id: task.id,
+      date: task.deadline,
+      in_past: task.deadline < Time.now,
+      name: task.decrypt(:content),
+      description: nil,
+      creator: assignee,
+      creation: task.creation,
+      anchor: task.anchor,
+    }
+  end
+
   def timeline_entries_for_case(case_)
     cid = case_
     cid = case_.id if case_.respond_to?(:id)
+    entries = []
 
-    entries = Kukupa::Models::CaseTimelineEntry.where(case: cid).map do |tl|
+    # actual timeline entries
+    entries << Kukupa::Models::CaseTimelineEntry.where(case: cid).map do |tl|
       timeline_entry(tl)
     end
 
+    # incomplete case tasks
+    entries << Kukupa::Models::CaseTask.where(case: cid, completion: nil).map do |ct|
+      timeline_entry_for_task(ct)
+    end
+
+    entries.flatten!
     entries.sort! {|a, b| a[:date] <=> b[:date]}
     entries
   end
