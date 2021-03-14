a09f4ffa891ad3683c5d7f2ecaa5dc092493e048
Lauren Jenkinson <lauren@iris.ac.nz>
2021-03-15 00:50:55 +1300

    case/task: Set task reminder worker to only send once

--- app/workers/case_task_reminder_worker.rb
+++ app/workers/case_task_reminder_worker.rb
@@ -7,7 +7,7 @@ class Kukupa::Workers::CaseTaskReminderWorker
 
     to_notify = []
     tasks = Kukupa::Models::CaseTask
-      .where(completion: nil)
+      .where(completion: nil, reminded: nil)
       .where{deadline > Sequel.function(:NOW)}
       .order(Sequel.desc(:creation))
       .all
