4f26d1eccf70aae5ee2479c134d5beca12aa653a
Iris System <iris@iris.ac.nz>
2021-09-09 14:36:49 +1200

    workers/caseunassignednewrequest: initialize
    This adds the background worker to scan for unassigned cases with new
    requests, inserting references to those in the CaseUnassignedNewRequest
    database table so that the dashboard can see them.
    
    This worker runs at 30 minutes past the hour, every 2 hours.

---
+++ app/workers/case_unassigned_new_request_worker.rb
@@ -0,0 +1,76 @@
+class Kukupa::Workers::CaseUnassignedNewRequestWorker
+  include Sidekiq::Worker
+
+  def perform
+    Kukupa.initialize if Kukupa.app.nil?
+    Kukupa.app_config_refresh(:force => true)
+
+    # For each case, check whether it has any assigned advocates.
+    # If it doesn't, grab it's ID, so we can look it up in the next step
+    logger.info("Getting cases with no advocates...")
+    case_ids = Kukupa::Models::Case.select(:id).all.map(&:id).map do |cid|
+      adv_count = Kukupa::Models::CaseAssignedAdvocate
+        .where(case: cid)
+        .count
+
+      next nil if adv_count.positive?
+      cid
+    end.compact.uniq
+
+    # For each case that we grabbed IDs for before, check if they have:
+    #
+    # - any new prisoner correspondence
+    # - any new outside requests
+    #
+    # If they don't, ignore the case.
+    logger.info("Checking request timestamps for #{case_ids.count} cases...")
+    cases = case_ids.map do |cid|
+      case_obj = Kukupa::Models::Case[cid]
+      next nil unless case_obj
+      timestamps = []
+
+      # Check correspondence
+      timestamps << Kukupa::Models::CaseCorrespondence
+        .select(:id, :case, :creation)
+        .where(case: case_obj.id)
+        .reverse(:creation)
+        .first
+        &.creation
+
+      # Check outside requests
+      timestamps << Kukupa::Models::CaseNote
+        .select(:id, :case, :creation, :is_outside_request)
+        .where(case: case_obj.id, is_outside_request: true)
+        .reverse(:creation)
+        .first
+        &.creation
+
+      # If no timestamps, ignore this case
+      timestamps.compact!
+      next nil if timestamps.empty?
+
+      # Get latest timestamp, use that
+      request_ts = timestamps.sort.last
+
+      {
+        case: case_obj,
+        request_ts: request_ts,
+      }
+    end.compact
+
+    # Now that we have our list of cases, clear the existing cases in the
+    # CaseUnassignedNewRequest table, and insert our updated ones
+    Kukupa::Models::CaseUnassignedNewRequest.select.delete
+    new_requests = cases.map do |cd|
+      cnr = Kukupa::Models::CaseUnassignedNewRequest.new({
+        case: cd[:case].id,
+        request_ts: cd[:request_ts],
+      })
+
+      cnr.save
+    end
+
+    logger.info("Generated #{new_requests.count} CaseUnassignedNewRequest objects")
+  end
+end
+
